3bfe7af0db428e468c71d797f96f6ca56205f9fc
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2023-07-10 00:05:49 +0200

    feature: Create Esercizio14

---
+++ Esercizio14_ArrayMethods.js
@@ -0,0 +1,66 @@
+function uncompletedNotes(notes) {
+    const uncompletedTodos = [];
+
+    for (const note of notes) {
+        for (const todo of note.todos) {
+            if (!todo.done) {
+            uncompletedTodos.push(todo);
+        }
+        }
+    }
+
+    return uncompletedTodos;
+}
+
+const notes = [
+    {
+        id: 1,
+        description: "Workout program",
+        todos: [
+        {
+            id: 1,
+            name: "Push ups - 10 x 3",
+            done: false,
+        },
+        {
+            id: 2,
+            name: "Abdominals - 20 x 3",
+            done: true,
+        },
+        {
+            id: 3,
+            name: "Tapis Roulant - 15min",
+            done: true,
+        },
+    ],
+    },
+    {
+        id: 2,
+        description: "Front-end Roadmap",
+        todos: [
+        {
+            id: 1,
+            name: "Learn HTML",
+            done: true,
+        },
+        {
+            id: 2,
+            name: "Learn CSS",
+            done: true,
+        },
+        {
+            id: 3,
+            name: "Learn JavaScript",
+            done: true,
+        },
+        {
+            id: 4,
+            name: "Learn Angular",
+            done: false,
+        },
+        ],
+    },
+];
+
+const uncompleted = uncompletedNotes(notes);
+console.log(uncompleted);
