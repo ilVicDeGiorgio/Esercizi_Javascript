750a02d4eda058b5c1c2089285ccc41b7ac36c2c
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2023-08-08 16:03:28 +0200

    feature: Created Esercizio26

---
+++ Esercizio26_Reduce.js
@@ -0,0 +1,11 @@
+function sum(...numbers) {
+    return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+console.log(sum(1, 2, 3, 4, 5));
+
+// function sum(num1, num2, num3, num4, num5) {
+//     return num1 + num2 + num3 + num4 + num5;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
